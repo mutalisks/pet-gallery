833d0268687c5f59e918e0836147adbd87622f0e
mutalisks <mutalisksg@gmail.com>
2023-03-03 08:38:13 +0800

    first version

---
+++ src/test/java/com/lgxq/petgallery/PetGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgxq.petgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
